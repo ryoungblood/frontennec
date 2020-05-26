59a4738ebc8b0b6699879f179d3c6fba1fd83818
youngblood <ryoungblood@pivotal.io>
2020-05-26 16:18:46 -0400

    static storybook
    adding static pages as a "push" test

---
+++ main.7c494d34b39fc1102421.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1338:function(n,o,c){"use strict";c.r(o);c(467),c(498)},515:function(n,o,c){c(516),c(661),c(1338),c(467),n.exports=c(498)},580:function(n,o){}},[[515,1,2]]]);
